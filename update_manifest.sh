5979f1ac3f5115a76ab9c32e1186c115328a683a
araman <artem01amosov@gmail.com>
2026-01-17 19:50:24 +0100

    update manifest script

--- update_manifest.sh
+++ update_manifest.sh
@@ -1,5 +1,7 @@
-docker manifest create ghcr.io/aramann/tg-api:latest \
+# 1. Создаем манифест (флаг --amend позволяет перезаписать существующий)
+docker manifest create --amend ghcr.io/aramann/tg-api:latest \
     ghcr.io/aramann/tg-api:amd64 \
     ghcr.io/aramann/tg-api:arm64
 
-docker manifest push ghcr.io/ваш_юзер/tg-api:latest
+# 2. Пушим манифест (исправлено имя юзера)
+docker manifest push ghcr.io/aramann/tg-api:latest
